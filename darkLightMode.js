ced964fee6dfc0756e76b708e30940519cd1405c
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 01:57:05 +1100

    created LightvsDark Mode Project

---
+++ darkLightMode.js
@@ -0,0 +1,5 @@
+const mode = document.querySelector('#switch')
+mode.addEventListener('click', function () {
+    document.body.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4f90510d-3dcd-4a9c-a1ee-5dbf41625fa8/df1awmr-b60fd344-b495-47f1-b175-89866df4b9ad.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRmOTA1MTBkLTNkY2QtNGE5Yy1hMWVlLTVkYmY0MTYyNWZhOFwvZGYxYXdtci1iNjBmZDM0NC1iNDk1LTQ3ZjEtYjE3NS04OTg2NmRmNGI5YWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WHTnJd_WIJKNAa9J4XLXiZvsxyOZ_igCnmyD_l3lONw')";
+    mode.firstChild.innerText = "Light"
+})

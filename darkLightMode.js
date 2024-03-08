e6d43c8b6fd9f2cde906ec962ff9383d17605c1d
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 02:01:12 +1100

    successfully created conditions for light and dark mode to be interchangeable

--- darkLightMode.js
+++ darkLightMode.js
@@ -1,5 +1,15 @@
 const mode = document.querySelector('#switch')
+let dark = true
 mode.addEventListener('click', function () {
-    document.body.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4f90510d-3dcd-4a9c-a1ee-5dbf41625fa8/df1awmr-b60fd344-b495-47f1-b175-89866df4b9ad.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRmOTA1MTBkLTNkY2QtNGE5Yy1hMWVlLTVkYmY0MTYyNWZhOFwvZGYxYXdtci1iNjBmZDM0NC1iNDk1LTQ3ZjEtYjE3NS04OTg2NmRmNGI5YWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WHTnJd_WIJKNAa9J4XLXiZvsxyOZ_igCnmyD_l3lONw')";
-    mode.firstChild.innerText = "Light"
+    if (dark) {
+        document.body.style.backgroundImage = "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4f90510d-3dcd-4a9c-a1ee-5dbf41625fa8/df1awmr-b60fd344-b495-47f1-b175-89866df4b9ad.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRmOTA1MTBkLTNkY2QtNGE5Yy1hMWVlLTVkYmY0MTYyNWZhOFwvZGYxYXdtci1iNjBmZDM0NC1iNDk1LTQ3ZjEtYjE3NS04OTg2NmRmNGI5YWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WHTnJd_WIJKNAa9J4XLXiZvsxyOZ_igCnmyD_l3lONw')";
+        mode.firstChild.innerText = "Light"
+        dark = false
+    }
+    else {
+        document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/676037.jpg')";
+        mode.firstChild.innerText = "Light"
+        dark = true
+    }
+
 })
